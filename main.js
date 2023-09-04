b14120841af19b90ec07b478210a4f5f7953c0b2
jujo2594 <105889688+jujo2594@users.noreply.github.com>
2023-09-03 22:31:59 -0500

    feat: sparkles: improving code

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let a = 'learning';
 let b = 'GIT';
+let c = 'GitHub'
 
 if(a !== 'learning'){
     console.log("You're not learing GIT");
 }else{
-    console.log(`You're ${a} ${b}`);
+    console.log(`You're ${a} ${b} and ${c}`);
 }
